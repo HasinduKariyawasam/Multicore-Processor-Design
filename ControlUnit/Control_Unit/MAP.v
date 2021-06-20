23d5053c298e15232a8436d323db22be7e0521d7
Kithmini Herath <kithminiherath9724@gmail.com>
2021-06-20 18:41:51 +0530

    Updated MAP and tested

--- ControlUnit/Control_Unit/MAP.v
+++ ControlUnit/Control_Unit/MAP.v
@@ -14,22 +14,22 @@ module MAP(clk,IR,out);
 
 	always @ (negedge clk)
 		case (map)
-			8'b00000001: out = 5'b00010;	// RSTALL
-			8'b00000010: out = 5'b00011;	// CONST
-			8'b00000011: out = 5'b00100;	// MOV
-			8'b00000100: out = 5'b00101;	// SIZE
-			8'b00000101: out = 5'b01010;	// JMPNZ
-			8'b00000110: out = 5'b01110;	// MOVMSB
-			8'b00000111: out = 5'b01111;	// MOVLSB
-			8'b00001000: out = 5'b10000;	// ADDX
-			8'b00001001: out = 5'b10001;	// ADDY
-			8'b00001010: out = 5'b10010;	// ADD
-			8'b00001011: out = 5'b10011;	// SUB
-			8'b00001100: out = 5'b10100;	// MUL
-			8'b00001101: out = 5'b10101;	// LOAD
-			8'b00001110: out = 5'b10111;	// STORE
-			8'b00001111: out = 5'b11001;	// INCI
-			8'b00010000: out = 5'b11010;	// RSTI
+			8'd0: out = 5'b00010;	// RSTALL
+			8'd16: out = 5'b00011;	// CONST
+			8'd32: out = 5'b00100;	// MOV
+			8'd48: out = 5'b00101;	// SIZE
+			8'd64: out = 5'b10011;	// SUB
+			8'd80: out = 5'b01010;	// JMPNZ
+			8'd96: out = 5'b01110;	// MOV02
+			8'd112: out = 5'b10000;	// ADDX
+			8'd128: out = 5'b10001;	// ADDY
+			8'd144: out = 5'b10100;	// MUL
+			8'd160: out = 5'b10010;	// ADD
+			8'd176: out = 5'b10101;	// LOAD
+			8'd192: out = 5'b01111;	// MOV13
+			8'd208: out = 5'b11011;	// INCI
+			8'd224: out = 5'b11000;	// STORE
+			8'd240: out = 5'b11100;	// RSTI
 		endcase
 	
 endmodule
---
+++ ControlUnit/Control_Unit/MAP_testbench.v
@@ -0,0 +1,35 @@
+`timescale 1us/10ns
+module MAP_testbench();
+
+	reg clk; // clock
+	reg [7:0] ir;
+	wire [4:0] out;
+	
+	MAP MAP1(clk,ir,out);
+	
+	always
+		begin
+			clk = 1; #50; clk = 0; #50;
+		end
+	
+	initial begin
+		ir = 8'd0; #200; // RSTALL
+		ir = 8'd16; #200; // CONST
+		ir = 8'd32; #200; // MOV
+		ir = 8'd48; #200; // SIZE
+		ir = 8'd64; #200; // SUB
+		ir = 8'd80; #200; // JMPNZ
+		ir = 8'd96; #200; // MOV02
+		ir = 8'd112; #200; // ADDX
+		ir = 8'd128; #200; // ADDY
+		ir = 8'd144; #200; // MUL
+		ir = 8'd160; #200; // ADD
+		ir = 8'd176; #200; // LOAD
+		ir = 8'd192; #200; // MOV13
+		ir = 8'd208; #200; // INCI
+		ir = 8'd224; #200; // STORE
+		ir = 8'd240; #200; // RSTI
+				
+	end
+
+endmodule
