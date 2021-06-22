ea0d088bd10b374cb64afc8f1401069534f7309f
Hasindu <khpiyumantha@gmail.com>
2021-06-23 02:10:47 +0530

    ALU updated

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -7,13 +7,13 @@ module ALU(input [15:0] ALU_IN1, ALU_IN2,
 	
 	always @ (*)
 		case (CTRL)
-			3'b000: result = ALU_IN1 + ALU_IN2;		//ADD
-			3'b001: result = ALU_IN1 - ALU_IN2;		//SUB
-			3'b010: result = ALU_IN1 * ALU_IN2;		//MUL
-			3'b011: result = {8'b0, ALU_IN2[7:0]};	//M8Z
-			3'b100: result = {14'b0, ALU_IN2[1:0]};	//M14Z {14'b0, ALU_IN2[1], ALU_IN2[3]}
-			3'b101: result = (ALU_IN2 >> 2);		//RSH2 {14'b0, ALU_IN2[0], ALU_IN2[2]}
-			3'b110: result = 16'b0;					//ZERO
+			3'b000: result = ALU_IN1 + ALU_IN2;					//ADD
+			3'b001: result = ALU_IN1 - ALU_IN2;					//SUB
+			3'b010: result = ALU_IN1 * ALU_IN2;					//MUL
+			3'b011: result = {8'b0, ALU_IN2[7:0]};				//M8Z
+			3'b100: result = {14'b0, ALU_IN2[1], ALU_IN2[3]};	//MOV13 
+			3'b101: result = {14'b0, ALU_IN2[0], ALU_IN2[2]};	//MOV02
+			3'b110: result = 16'b0;								//ZERO
 		endcase
 		
 	assign ALU_OUT = result;
