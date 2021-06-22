d2704e799bc2851b7a147638e775561c1d2493e2
Kithmini Herath <kithminiherath9724@gmail.com>
2021-06-22 12:51:08 +0530

    Added Registers 1,2,5 and AC

---
+++ Registers/R15/Reg15.v
@@ -0,0 +1,41 @@
+module Reg15(clk,BIN,RST,WR,LDBUS,LDALU,BOUT,ALU);
+	/*
+		Control signals:
+		clk - Clock signal
+		RST - RESET control signal
+		WR - Control signal to write to register
+		LDBUS - Control signal to load content in the register to the BUS
+		LDALU - Control signal to load content in the register to the ALU via ALUMUX
+		
+		Datapaths:
+		BIN - The BUS to write to the register
+		BOUT - The BUS to read content from the register
+		ALU - The wire to the MUX from the register
+	*/
+
+	input clk, RST, WR, LDBUS, LDALU;     // Clock and Control Signals
+	input [15:0] BIN;
+	output [15:0] BOUT;
+	output [15:0] ALU;
+	
+	reg unsigned [15:0] register;	
+	reg unsigned [15:0] BOUT;					// output to BUS
+	reg unsigned [15:0] ALU;					// output to ALU
+
+	
+	always @ (posedge clk)
+		begin
+			if (RST == 1)
+				register <= 0;
+			else if (WR == 1)
+				register <= BIN;
+		end
+		
+	always @ (~clk)
+		begin
+			if (LDBUS == 1)
+				BOUT = register;
+			else if (LDALU == 1)
+				ALU = register;
+		end
+endmodule
---
+++ Registers/R2/R2.v
@@ -0,0 +1,40 @@
+module R2(clk,BIN,RST,RST2,WR,LDBUS,INC,BOUT);
+	/*
+		Control signals:
+		clk - Clock signal
+		RST - Common RESET control signal
+		RST2 - RESET signal to only reset R2
+		WR - Control signal to write to register
+		LDBUS - Control signal to load content in the register to the BUS
+		INC - Increment R2 by 1
+		
+		Datapaths:
+		BIN - The BUS to write to the register
+		BOUT - The BUS to read content from the register
+		ALU - The wire to the MUX from the register
+	*/
+
+	input clk, RST, RST2, WR, LDBUS, INC;     // Clock and Control signals
+	input [15:0] BIN;
+	output [15:0] BOUT;
+	
+	reg unsigned [15:0] reg2;
+	reg unsigned [15:0] BOUT;	// output to BUS
+	
+	always @ (posedge clk)
+		begin
+			if (RST == 1 || RST2 == 1)
+				reg2 <= 0;
+				
+			if (INC == 1)
+				reg2 <= reg2 + 1;
+			else if (WR == 1)
+				reg2 <= BIN;
+		end
+		
+	always @ (~clk)
+		begin
+			if (LDBUS == 1)
+				BOUT = reg2;
+		end
+endmodule
