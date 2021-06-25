9cdda69e816c911c4977718211b69b24ec7ef862
Harindu <harindu.ravin@gmail.com>
2021-06-25 21:11:00 +0530

    Modified AR register then added its testbench

---
+++ Registers/AR/RegAR_testbench.v
@@ -0,0 +1,24 @@
+`timescale 1us/10ns
+module RegAR_testbench();
+
+	reg clk; 
+	reg WR;
+	reg [15:0] BIN;
+	wire [15:0] DMADDR;
+	
+	AR_register AR_register1(BIN,clk,WR,DMADDR);
+	
+	always // CLOCK
+		begin
+			clk = 1; #50; clk = 0; #50;
+		end
+	
+	initial begin
+		#50;
+		WR=1'b1; BIN = 16'h00FF; #100; // WRITE to AC
+		WR=1'b0; BIN = 16'hFFFF; #100; // LOAD BUS
+		WR=1'b1; BIN = 16'h00CC; #100; // WRITE to AC
+		WR=1'b0; BIN = 16'h0EEF; #100; // LOAD BUS
+	end
+
+endmodule
