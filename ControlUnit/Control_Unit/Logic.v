7fc470d38837615b2d5f2ad63aa95b710917a877
Kithmini Herath <kithminiherath9724@gmail.com>
2021-06-26 10:13:41 +0530

    Added start condition to Logic

--- ControlUnit/Control_Unit/Logic.v
+++ ControlUnit/Control_Unit/Logic.v
@@ -8,16 +8,19 @@ module Logic(Z,IR,naddr,br,out);
 	reg [4:0] out;
 	
 	always @ (*) // activate whenever an input changes
-		// how to implement the start??? currently IR all ones is mapped to FETCH1 in MAP.
-		// but for the above to work the branch bit will have to be set to 1 at the very start of the system
+		// currently IR all ones is mapped to FETCH1 in MAP.
 		begin
-			if (br == 0)
-				out = naddr;
-			else
-				if ((Z == 1) & (IR == 5'b01001))				// to quit JUMP
-					out = 5'b01011;
-				else													// any other instruction or to JUMP
-					out = IR;
-				end
+			if (IR == 5'b00000) // START of the machine
+				out = IR;
+			else begin
+				if (br == 0)
+					out = naddr;
+				else
+					if ((Z == 1) & (IR == 5'b01001))				// to quit JUMP
+						out = 5'b01011;
+					else													// any other instruction or to JUMP
+						out = IR;
+					end
+			end
 
 endmodule
