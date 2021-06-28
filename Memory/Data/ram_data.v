aac8399da940b29362f47fee33aa6a217c99a9b7
Harindu <harindu.ravin@gmail.com>
2021-06-28 18:00:24 +0530

    Added duel core memory

--- Memory/Data/ram_data.v
+++ Memory/Data/ram_data.v
@@ -1,22 +1,22 @@
-module ram_data(DATAOUT,ADDBUS,DATAIN,WR, RD, clk);
+module ram_data(DATAOUT_1,ADDBUS_1,DATAIN_1,WR_1, RD_1, DATAOUT_2,ADDBUS_2,DATAIN_2,WR_2, RD_2, clk);
 /*
-	Control signals:
+	Control signals: For duel core access following are duplicated and indexed by 1 and 2
 	
 	clk - Clock signal
 	WR - Write to memory
 	RD - Read from memory
 		
-	Datapaths:
-	
+	Datapaths: For duel core access following are duplicated and indexed by 1 and 2
+
 	DATAOUT - 16 bit data output from memory
 	ADDBUS - 16 bit memory address
 	DATAIN - 16 bit data input to the memory
 */
 
-   output reg [15:0] DATAOUT;
-   input [15:0] DATAIN;
-   input [15:0] ADDBUS;
-   input WR,RD, clk;
+   output reg [15:0] DATAOUT_1, DATAOUT_2;
+   input [15:0] DATAIN_1, DATAIN_2;
+   input [15:0] ADDBUS_1, ADDBUS_2;
+   input WR_1,RD_1, WR_2, RD_2, clk;
    reg [15:0] mem [1023:0];
 
 	initial begin
@@ -26,22 +26,37 @@ module ram_data(DATAOUT,ADDBUS,DATAIN,WR, RD, clk);
 	end
 
     always @(posedge clk) begin
-			if (WR) begin
-				mem[ADDBUS] <= DATAIN;
+			if (WR_1) begin
+				mem[ADDBUS_1] <= DATAIN_1;
+			end
+			if (WR_2) begin
+				mem[ADDBUS_2] <= DATAIN_2;
 			end
 			$writememb("D:/FPGA/Multicore Processor Design/Memory/Data/DATA_OUT.txt",mem,0,50);    	
    end
 	
 	
-	always @ (RD)
+	always @ (RD_1)
 		begin
-			if (RD == 1)
+			if (RD_1 == 1)
 				begin
-					DATAOUT = mem[ADDBUS];
+					DATAOUT_1 = mem[ADDBUS_1];
 				end
 			else 
 				begin
-					DATAOUT = 16'bz;
+					DATAOUT_1 = 16'bz;
+				end
+		end
+
+	always @ (RD_2)
+		begin
+			if (RD_2 == 1)
+				begin
+					DATAOUT_2 = mem[ADDBUS_2];
+				end
+			else 
+				begin
+					DATAOUT_2 = 16'bz;
 				end
 		end
 endmodule
