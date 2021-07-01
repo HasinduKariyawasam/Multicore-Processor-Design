c658575f1ad2777d6c404fb222afe41ecd95b252
Harindu <harindu.ravin@gmail.com>
2021-07-01 16:08:48 +0530

    modified register controller

--- MemoryController/Register_controller/register_ctrl_testbench.v
+++ MemoryController/Register_controller/register_ctrl_testbench.v
@@ -4,11 +4,12 @@ module register_ctrl_testbench();
 	reg clk; 
 	reg state_ctrl_store;
 	wire [3:0] rowaddr;
+	wire [15:0] sw2_out;	
 	wire writemem;
 	wire state_ctrl_done;
 
 	
-	register_ctrl dut(clk,state_ctrl_store,rowaddr,writemem,state_ctrl_done);
+	register_ctrl dut(clk,state_ctrl_store,rowaddr, sw2_out, writemem,state_ctrl_done);
 	
 	always // CLOCK
 		begin
@@ -18,9 +19,9 @@ module register_ctrl_testbench();
 	initial begin
 		state_ctrl_store=1'b0;
 		#250;
-		state_ctrl_store=1'b1; #100;state_ctrl_store = 1'b0;
+		state_ctrl_store=1'b1; #100;state_ctrl_store = 1'b0; // WRITE to AR
 		#600;
-		state_ctrl_store=1'b1; #100;state_ctrl_store = 1'b0;
+		state_ctrl_store=1'b1; #100;state_ctrl_store = 1'b0; // WRITE to AR
 		#600;		
 		$finish;
 	end
